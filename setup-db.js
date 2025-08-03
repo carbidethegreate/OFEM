89545ee44aded0f427e2369da9561282174a4819
carbidethegreate <whitakerdavid@pm.me>
2025-08-03 13:42:30 -0400

    Load environment variables in setup DB script

--- setup-db.js
+++ setup-db.js
@@ -9,6 +9,9 @@ const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
 const { exec } = require('child_process');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const createTableQuery = `
 CREATE TABLE IF NOT EXISTS fans (
